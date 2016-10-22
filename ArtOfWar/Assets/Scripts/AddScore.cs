a7947e7a518e6515366eb8f25f8f25d965748e76
kanaircla <zhangkun0062@126.com>
2016-10-22 21:40:22 +0800

    modify

---
+++ ArtOfWar/Assets/Scripts/AddScore.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddScore : MonoBehaviour {
+	private GUIText mGUIText;
+	private GUIText mGUIText2;
+
+	void Start() {
+		mGUIText = gameObject.GetComponent<GUIText> ();
+		mGUIText2 = gameObject.GetComponent<GUIText> ();
+	}
+
+	public void addScore(){
+		int score = int.Parse(mGUIText.text);
+		score++;
+		mGUIText.text = score.ToString();
+	}
+}
